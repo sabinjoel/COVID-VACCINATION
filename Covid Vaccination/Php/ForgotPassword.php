cd6285b8e8bc572f87b8e3ecb21ff351550a7e15
R SABIN JOEL <87659054+sabinjoel@users.noreply.github.com>
2022-08-14 00:06:59 +0530

    Delete Covid Vaccination directory

--- Covid Vaccination/Php/ForgotPassword.php
+++
@@ -1,7 +0,0 @@
-<!-- 
-<?php
-    $msg = "Sample Forgot Password For Car Rental System";
-    $headers = "From : pavanharsha987@gmail.com";
-    mail("akhilgorle06@gmail.com@","Testing",$msg,$headers);
-    echo "Done" ?> 
--->
